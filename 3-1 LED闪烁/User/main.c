a0835a68a698c583da1aa8b80a0008b5288cde88
Luo Jia Wei <251500892@qq.com>
2024-06-27 09:50:47 +0800

    feat: GPIO输出

---
+++ 3-1 LED闪烁/User/main.c
@@ -0,0 +1,31 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+int main()
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	
+	while(1)
+	{
+		GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+		Delay_ms(500);
+		GPIO_SetBits(GPIOA, GPIO_Pin_0);
+		Delay_ms(500);
+		
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);	//点亮LED
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);		//熄灭LED
+		Delay_ms(500);
+		
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)0);	//点亮LED
+		Delay_ms(500);
+		GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)1);	//熄灭LED
+		Delay_ms(500);
+	}
+}
